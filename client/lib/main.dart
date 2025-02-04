20919af3f7da54f441b55040c1cc306c6a48af57
Adedola Owen Abaru <56795990+Software78@users.noreply.github.com>
2025-02-04 15:57:53 +0100

    Implement user login and registration forms with error handling

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -20,28 +20,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _dio = dio.Dio(dio.BaseOptions(baseUrl: dotenv.env['BASE_URL'] ?? ''))
-    ..options.connectTimeout = const Duration(minutes: 1)
-    ..options.receiveTimeout = const Duration(minutes: 1)
-    ..options.sendTimeout = const Duration(minutes: 1)
-    ..interceptors.add(EncryptionInterceptor())
-    ..interceptors.add(
-      PrettyDioLogger(
-        requestBody: true,
-        requestHeader: true,
-        responseHeader: true,
-        logPrint: (value) {
-          if (kDebugMode) {
-            log(value.toString(), name: 'Dio');
-          }
-        },
-      ),
-    );
-
-  Future<void> register() async {}
-
-  Future<void> login() async {}
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -65,6 +43,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _loginEmailController = TextEditingController();
+  final _loginPasswordController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+
+  final _dio = dio.Dio(dio.BaseOptions(baseUrl: dotenv.env['BASE_URL'] ?? ''))
+    ..options.connectTimeout = const Duration(minutes: 1)
+    ..options.receiveTimeout = const Duration(minutes: 1)
+    ..options.sendTimeout = const Duration(minutes: 1)
+    ..interceptors.add(EncryptionInterceptor())
+    ..interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        requestHeader: true,
+        responseHeader: true,
+        logPrint: (value) {
+          if (kDebugMode) {
+            log(value.toString(), name: 'Dio');
+          }
+        },
+      ),
+    );
+
+  Future<void> register() async {
+    setState(() {
+      registerLoading = true;
+    });
+    try {
+      final response = await _dio.post('/auth/register', data: {
+        'email': _emailController.text,
+        'password': _passwordController.text,
+        'first_name': _firstNameController.text,
+        'last_name': _lastNameController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User registered: ${response.data['data']}'),
+        ),
+      );
+    } on dio.DioException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User logged in: ${e.toString()}'),
+        ),
+      );
+      log('Error logging in: $e', name: 'Login');
+    }
+    setState(() {
+      registerLoading = false;
+    });
+  }
+
+  Future<void> login() async {
+    setState(() {
+      loginLoading = true;
+    });
+    try {
+      final response = await _dio.post('/auth/login', data: {
+        'email': _loginEmailController.text,
+        'password': _loginPasswordController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User logged in: ${response.data['data']}'),
+        ),
+      );
+    } on dio.DioException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User logged in: ${e.toString()}'),
+        ),
+      );
+      log('Error logging in: $e', name: 'Login');
+    }
+    setState(() {
+      loginLoading = false;
+    });
+  }
+
+  bool loginLoading = false;
+  bool registerLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,7 +136,70 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: PageView(
         children: [
-          Column(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _loginEmailController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _loginPasswordController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'Password'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: loginLoading ? null : login,
+                  child: const Text('Login'),
+                ),
+                SizedBox(height: 40),
+                TextFormField(
+                  controller: _emailController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'Password'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _firstNameController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'First Name'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _lastNameController,
+                  onTapOutside: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                  decoration: const InputDecoration(labelText: 'Last Name'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: registerLoading ? null : register,
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
